1f03fd31b90179c4b19f186d298e50fc3a3c8794
Cameron Bytheway <cameron@nujii.com>
2013-07-13 00:23:53 -0600

    Resolve pusher path

--- src/cowboy_pusher.erl
+++ src/cowboy_pusher.erl
@@ -3,18 +3,22 @@
 -export([execute/2]).
 
 execute(Req, Env) ->
-  Req2 = publish(cowboy_req:meta(pub_event, Req)),
-  {ok, Req2, Env}.
+  {Path, Req2} = cowboy_req:path(Req),
+  {QS, Req3} = cowboy_req:qs(Req2),
+  URL = case QS of
+    <<>> -> Path;
+    QS -> <<Path/binary,"?",QS/binary>>
+  end,
+  Req4 = publish(cowboy_base:resolve(URL, Req3), cowboy_req:meta(pub_event, Req3)),
+  {ok, Req4, Env}.
 
-publish({{Event, Data}, Req}) ->
-  {URL, Req2} = cowboy_req:url(Req),
+publish(URL, {{Event, Data}, Req}) ->
   Channel = websaferl:encode(URL),
   pusherl:push_async(Channel, Event, Data),
-  Req2;
-publish({Event, Req}) when is_binary(Event) ->
-  {URL, Req2} = cowboy_req:url(Req),
+  Req;
+publish(URL, {Event, Req}) when is_binary(Event) ->
   Channel = websaferl:encode(URL),
   pusherl:push_async(Channel, Event, <<>>),
-  Req2;
-publish({_, Req}) ->
+  Req;
+publish(_, {_, Req}) ->
   Req.
